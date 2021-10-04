097fed4dcbcb5699ba8682ef8d2f14541f732a24
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2021-10-04 09:24:59 +0200

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,29 +1,45 @@
 import socket
+import json
+import sys
 
 class Server:
     @staticmethod
     def run():
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(('127.0.0.1', 9999))
-            s.listen()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('127.0.0.1', 9999))
+        s.listen()
+
+        while True:
+            # await connections
+            clientsocket, address = s.accept() 
+            print('Connected by', address)
 
             while True:
-                # await connections
-                clientsocket, address = s.accept() 
-                print('Connected by', address)
+                data = clientsocket.recv(1024)
 
-                while True:
-                    data = clientsocket.recv(1024)
+                if not data:
+                    break
 
-                    if not data:
-                        break
+                if data['request'] == 'search_torrents':
+                    # search torrents matching data['query']
+                    # use beautiful soup?
+                    # return list of results
+                    msg = { 'results' : [] }
 
-                    # TODO: look at data, read the request, handle request
-                    print(data)
+                elif data['request'] == 'status_check':
+                    # make status check to transmission
+                    # return status
+                    msg = { 'status' : 0 }
 
-                    # like this we can send something back 
-                    clientsocket.sendall(data)
-       
+                data = json.dumps(msg)
+                try:
+                    clientsocket.sendall(bytes(data, encoding="utf-8"))
+                except socket.error:
+                    print("Failed to send")
+                    sys.exit()
+
+        s.close()
+
     def search_torrents():
         # TODO: enable VPN
         # TODO: send https req
