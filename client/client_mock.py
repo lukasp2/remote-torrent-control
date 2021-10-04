f79ff87bde0d5c3f20315791682c82ec18ea08ac
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2021-10-04 09:25:34 +0200

    Update client_mock.py

--- client/client_mock.py
+++ client/client_mock.py
@@ -2,18 +2,28 @@
 
 import socket
 import sys
+import json
 
 class Client:
-
     @staticmethod
     def run():        
-        HOST = '127.0.0.1'  # The server's hostname or IP address
-        PORT = 9999         # The port used by the server
+        HOST = '127.0.0.1'  # server's hostname or IP address
+        PORT = 9999         # server's port
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
+
+            # input query you would like to mock
+            x = int(input())
+
+            if x == 1:
+                msg = {"request": "search_torrents", "query": "batman"}
+            elif x == 2:
+                msg = {"request": "status_check"}
+            
+            data = json.dumps(msg)
             try:
-                s.sendall(b'Hello, world')
+                s.sendall(bytes(data, encoding="utf-8"))
             except socket.error:
                 print("Failed to send")
                 sys.exit()
